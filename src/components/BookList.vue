8970aa62ec1711510aadf149466b6deb3a203628
yitzhak Matias <matias6@gmail.com>
2019-10-20 01:39:02 -0400

    - first exercise

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,52 +4,58 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
-    <br><hr>
+    <br>
+    <hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
-import BookItem from "./BookItem";
-import BookForm from "./BookForm";
+    import BookItem from "./BookItem";
+    import BookForm from "./BookForm";
 
-export default {
-  name: "BookList",
-  data() {
-    return {
-      title: "All Books",
-      states: ["Want to Read", "Read", "Reading"],
-      books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
-      ]
+    export default {
+        name: "BookList",
+        data() {
+            return {
+                title: "All Books",
+                states: ["Want to Read", "Read", "Reading"],
+                books: [
+                    {title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true},
+                    {title: "American Gods", author: "Neil Gaiman", finishedReading: false},
+                    {title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true}
+                ]
+            };
+        },
+        components: {
+            BookItem,
+            BookForm
+        },
+        methods: {
+            appendBook(bookData) {
+                this.books.push({
+                    title: bookData.bookTitle,
+                    author: bookData.bookAuthor,
+                    finishedReading: bookData.finishedReading
+                });
+            }
+        }
     };
-  },
-  components: {
-    BookItem,
-    BookForm
-  },
-  methods: {
-    appendBook(bookTitle, bookAuthor) {
-      this.books.push({ title: bookTitle, author: bookAuthor });
-    }
-  }
-};
 </script>
 
 <style>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 
-li {
-  display: block;
-  margin: 0 10px;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: block;
+    margin: 0 10px;
+  }
 </style>
